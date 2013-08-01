497317a183f0c79a76908171e257c577e324a6ee
Kaben G. Nanlohy <kaben@Kaa.local>
2013-07-31 21:39:59 -0500

    Added basic cStrand functionality and tests.

--- source/cpu/cStrand.h
+++ source/cpu/cStrand.h
@@ -22,7 +22,52 @@
 #define cStrand_h
 
 #include "cCPUMemory.h"
+#include "tList.h"
 
+class cStrand;
+class cStrandLabelPositions;
 
+/*
+ A strand is at heart just a string. But we're going to add per-strand label
+ tracking. A label is a substring consisting of certain special characters.
+ */
+class cStrand : public cCPUMemory {
+public:
+  Apto::Map<int, Apto::Array<int> > m_label_ids_by_position;
+  Apto::Map<int, cStrandLabelPositions > m_label_positions_by_id;
+public:
+  cStrand(const cStrand& in_strand) : cCPUMemory(in_strand) {}
+  cStrand(const cCPUMemory& in_memory) : cCPUMemory(in_memory) {}
+  cStrand(const InstructionSequence& in_genome) : cCPUMemory(in_genome) {}
+  cStrand(const Apto::String& in_string) : cCPUMemory(in_string) {}
+  explicit cStrand(int size = 1) : cCPUMemory(size) {}
+  ~cStrand() { ; }
+};
+
+/*
+ There can be several copies of each label in a given strand, each copy
+ appearing at a different position. For each label ID, the corresponding
+ cStrandLabelPositions records the locations of each copy of the label.
+ 
+ The strand's label index is a hashmap of these cStrandLabelPositions records,
+ and is keyed by the distinct label IDs.
+ */
+class cStrandLabelPositions {
+public:
+  Apto::Array<int> m_label_positions;
+  /*
+   m_strand_index_node is initially set to NULL. When the owning strand is
+   placed in the main index, backrefs into the index are stored in
+   m_strand_index_node, one backref for each label-ID. The backrefs are
+   recorded so we can quickly find the parts of the index that need updating
+   when we want to remove the strand from the index.
+   */
+  tListNode<cStrand> *m_index_backref;
+public:
+  cStrandLabelPositions(int num_positions = 0)
+  : m_label_positions(num_positions)
+  , m_index_backref(0)
+  {}
+};
 
 #endif
--- unittests/core/Strand.cc
+++ unittests/core/Strand.cc
@@ -28,7 +28,28 @@
 
 
 namespace nStrandBrainstorming {
-  TEST(cStrand, Brainstorm) {
-    EXPECT_EQ(1, 1);
+  TEST(cStrand, strand_default_instantiation) {
+    /*
+     A default strand is essentially an empty string. Elsewhere in Avida,
+     the convention is to initialize these default sequences to have length 1,
+     so we're following that convention.
+     */
+    cStrand *strand = new cStrand();
+    EXPECT_TRUE(strand);
+    EXPECT_EQ(strand->GetSize(), 1);
+    delete strand;
+  }
+  TEST(cStrand, strand_string_instantiation) {
+    /*
+     We're only going to test one other kind of instantiation:
+     with a string argument. Since cStrand calls its superclass' constructors,
+     which should be tested elsewhere,
+     we're going to skip the rest of those tests here.
+     */
+    cStrand *strand = new cStrand(Apto::String("abcdxyzabcdxyzabcd"));
+    EXPECT_TRUE(strand);
+    EXPECT_EQ(strand->AsString(), Apto::String("abcdxyzabcdxyzabcd"));
+    EXPECT_EQ(strand->GetSize(), sizeof("abcdxyzabcdxyzabcd")-1);
+    delete strand;
   }
 };
